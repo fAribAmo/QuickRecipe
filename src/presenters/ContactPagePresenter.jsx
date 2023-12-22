26650f330ad21bd95d700710985b2bd91ec8e32e
Lellalu <lella.lu14@outlook.com>
2023-12-22 18:42:04 +0100

    update third party component: react-final-form

--- src/presenters/ContactPagePresenter.jsx
+++ src/presenters/ContactPagePresenter.jsx
@@ -1,6 +1,5 @@
 import { observer } from "mobx-react-lite";
 import ContactPageView from "../views/ContactPageView";
-import { useNavigate } from 'react-router-dom';
 import React from 'react';
 
 export default 
